39ee1120199d5a29771e8e06660d2cba2a678d6c
felix-717 <vaghs622@gmail.com>
2025-02-20 15:34:35 -0700

    adds create and deletes note

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from api.serializers import UserSerializer
+from api.serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+class NoteListCreate(generics.ListCreateAPIView):
+    
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class NoteDelete(generics.DestroyAPIView):
+    
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
+
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
