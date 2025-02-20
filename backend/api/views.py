2bbef88f70a3ad0347f29aab5eadc002bbf81f2c
felix-717 <vaghs622@gmail.com>
2025-02-20 15:26:57 -0700

    adds note model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from api.serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    
+    #gets all record in the db 
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    
+    
