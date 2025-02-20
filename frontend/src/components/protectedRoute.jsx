94d0375ca910e0cb2020231d3bc528715d976861
felix-717 <vaghs622@gmail.com>
2025-02-20 16:16:28 -0700

    adds protectedrotue

---
+++ frontend/src/components/protectedRoute.jsx
@@ -0,0 +1,59 @@
+import {Navigate} from "react-router-dom"
+import {useAuth} from "../hooks/useAuth"
+import api from "../api"
+import {ACCESS_TOKEN, REFRESH_TOKEN} from "../constants"
+import { useState } from "react"
+
+
+function ProtectedRoute({children}) {
+    const {isAuthorized, setisAuthorized} = useState(null)
+
+    const refreshToken = async () => {
+      const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+      try {
+        const res =await api.post("api/post/refresh/", {refresh: refreshToken});
+        if (res.status === 200) {
+          localStorage.setItem(ACCESS_TOKEN, res.data.access)
+          setisAuthorized(true)
+        }
+        else {
+          setisAuthorized(false)
+        }
+      } catch (error) {
+        console.log(error)
+        setisAuthorized(false)
+        
+      }
+    }
+
+    const auth = async () => {
+    // get token 
+    const token = localStorage.getItem(ACCESS_TOKEN)
+    if (!token) {
+        setisAuthorized(false)
+        return
+    }
+    const decoded = jwtDecode(token)
+    const tokenExpiration = decoded.exp
+    const now = Date.now() / 1000
+
+    if (tokenExpiration < now) {
+        setisAuthorized(false)
+        return
+    }
+    setisAuthorized(true)
+}
+
+   
+
+    if (isAuthorized === null) {
+     return <div>Loading...</div>
+    }
+
+
+    return isAuthorized ? children : <Navigate to="/login" />
+
+
+}
+
+export default ProtectedRoute
